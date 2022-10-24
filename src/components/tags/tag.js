4c0b52fae34b20e8926fe653ef757c8ac8ce9b02
Kelly Francoeur <kellyafrancoeur@gmail.com>
2022-10-24 13:32:47 -0500

    tag management completed

--- src/components/tags/tag.js
+++ src/components/tags/tag.js
@@ -1,6 +1,23 @@
-export const Tag = ({ label }) => {
+import { Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
+export const Tag = ({ label }) => {
+    const navigate = useNavigate();
     return <>
         <h2>Label: {label}</h2>
+        <Button
+            variant="dark"
+            className="back__button"
+            onClick={() => navigate(`/`)}
+            >
+            Edit
+          </Button>
+          <Button
+            variant="dark"
+            className="back__button"
+            onClick={() => navigate(`/`)}
+            >
+            Delete
+          </Button>
     </>
 }
